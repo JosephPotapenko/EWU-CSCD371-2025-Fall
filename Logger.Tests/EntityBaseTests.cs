66bdd6af71be80e36141e9ad954f416e21deeb39
Joseph Potapenko <129787506+JosephPotapenko@users.noreply.github.com>
2025-10-16 23:00:28 -0700

    TDD EntityBase miimal test and build

---
+++ Logger.Tests/EntityBaseTests.cs
@@ -0,0 +1,22 @@
+﻿using System;   
+using Xunit;
+
+namespace Logger.Tests;
+
+public class EntityBaseTests
+{
+    [Fact]
+    public void EntityBase_DerivedProvidesNameAndId_SetsProperties()
+    {
+        Guid id = new();
+        TestDerived derived = new TestDerived { Id = id };
+
+        Assert.Equal(id, derived.Id);
+        Assert.Equal("Test Derived", derived.Name);
+    }
+
+    private class TestDerived : Logger.EntityBase
+    {
+        public override string Name => "Test Derived";
+    }
+}
---
+++ Logger/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Logger;
+
+/// <summary>
+/// Abstract base that implements IEntity.Id but leaves Name to be provided by derived classes.
+/// </summary>
+public abstract class EntityBase : IEntity
+{
+    public Guid Id { get; init; }
+
+    // Do not implement Name here. Make derived classes to provide it.
+    public abstract string Name { get; }
+}
