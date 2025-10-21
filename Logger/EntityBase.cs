bc88d63f64cf634933f732709c1c54b0ed78c94f
Joseph Potapenko <129787506+JosephPotapenko@users.noreply.github.com>
2025-10-21 00:50:31 +0000

    Quick fix

--- Logger/EntityBase.cs
+++ Logger/EntityBase.cs
@@ -5,11 +5,11 @@ namespace Logger;
 /// <summary>
 /// Abstract base that implements IEntity.Id but leaves Name to be provided by derived classes.
 /// </summary>
-public abstract class EntityBase : IEntity
+public abstract record EntityBase : IEntity
 {
     // Id: implemented here implicitly to provide a single init-only Guid Id implementation for all derived entities.
     public Guid Id { get; init; }
 
-    // Name: left abstract so derived classes must provide a calculated, backing-field-free implementation.
+    // Name: left abstract so derived records must provide a calculated, backing-field-free implementation.
     public abstract string Name { get; }
 }
