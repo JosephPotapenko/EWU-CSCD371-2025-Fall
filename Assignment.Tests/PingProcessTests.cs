cfb7b8bd61a0ef4d2b9b574ea444b9d6921a52fb
BillMillerCoding <billmillercoding@outlook.com>
2025-12-02 12:29:24 -0800

    added some waring ignores

--- Assignment.Tests/PingProcessTests.cs
+++ Assignment.Tests/PingProcessTests.cs
@@ -76,12 +76,13 @@ public class PingProcessTests
             "can't resolve",
             "badaddress"
         };
-
+#pragma warning disable CA2249
         bool containsKnownFragment = knownErrorFragments.Any(f =>
             stdOutput.IndexOf(f, StringComparison.OrdinalIgnoreCase) >= 0);
 
         Assert.IsTrue(containsKnownFragment, $"Output is unexpected: {stdOutput}");
         Assert.AreNotEqual<int>(0, exitCode);
+#pragma warning restore CA2249
     }
 
     [TestMethod]
